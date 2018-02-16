f160b354966a059b4b40439acb1d0af831a2a9f7
Ghitodin <ghitodin@gmail.com>
2018-02-16 03:50:04 +0200

    Ui loading fixed

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import PyQt5.QtCore
-from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QDialog, QMainWindow
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+from ui.autogen_ui.MainWindow import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        loadUi()
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+a = QApplication(sys.argv)
+main_window = MainWindow()
+main_window.show()
+sys.exit(a.exec())
